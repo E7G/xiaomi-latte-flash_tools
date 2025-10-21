8033b4bbf50a67abc3e0167167784c8e0b42f8f0
Qs315490 <qs315490@qq.com>
2025-10-21 14:53:37 +0800

    更新 .gitignore 文件，添加对 images 目录和压缩文件的忽略；在 DNX_flash_all.bat 中添加 OEM 变量flash命令

--- DNX_flash_all.bat
+++ DNX_flash_all.bat
@@ -17,6 +17,10 @@ fastboot getvar product 2>&1 | findstr /r /c:"^product: *latte" || exit /B 1
 fastboot oem unlock
 if %debug% == 1  Pause
 
+fastboot flash oemvars %~dp0images\oemvars.txt
+fastboot flash oemvars %~dp0images\oemvars-battery-config-fake-disabled.txt
+fastboot flash oemvars %~dp0images\oemvars-battery-config-fake.txt
+
 fastboot flash gpt %~dp0images\gpt.bin
 fastboot flash boot  %~dp0images\xiaomi-latte-boot.img
 fastboot flash system  %~dp0images\xiaomi-latte-root.img
