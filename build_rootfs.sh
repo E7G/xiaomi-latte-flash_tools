73b832778e401921efc8d24285935877295ca62d
QuickSwift315490 <qs315490@qq.com>
2025-10-30 13:53:05 +0800

    Fix fstab entry removal for swapfile

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -335,7 +335,7 @@ EOF
 	genfstab -U $mount_dir >> $mount_dir/etc/fstab
 	# fix fstab
 	sed -i 's/\\0[^ ]*//' $mount_dir/etc/fstab
-	sed -i '|/mnt/nvme/swap/swapfile|d' $mount_dir/etc/fstab
+	sed -i '/swapfile/d' $mount_dir/etc/fstab
 
 	echo 链接 vi 到 vim
 	run ln -s /usr/bin/vim /usr/bin/vi
