5a0383e7fe46e0f3dcdc72bf6903de0aeaf3c0aa
QuickSwift315490 <qs315490@qq.com>
2025-12-11 23:36:23 +0800

    Fix sed command to handle tab characters in fstab

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -333,7 +333,7 @@ EOF
 	echo 生成 fstab
 	genfstab -U $mount_dir >> $mount_dir/etc/fstab
 	# fix fstab
-	sed -i 's/\\0[^ ]*//' $mount_dir/etc/fstab
+	sed -i 's/\\0[^ \t]*//' $mount_dir/etc/fstab
 	sed -i '/swapfile/d' $mount_dir/etc/fstab
 
 	echo 链接 vi 到 vim
